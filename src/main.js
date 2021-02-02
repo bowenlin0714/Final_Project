0ea8f788fdcdfc67b43ff8696619fdb9bedec592
bowen125125 <bowen125125@gmail.com>
2021-02-02 10:01:39 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -33,7 +33,6 @@ import 'echarts/lib/component/tooltip'
 // import swal from 'sweetalert2'
 
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
